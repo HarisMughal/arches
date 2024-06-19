641a0dbeac98ec2cba4d6da26f093e9e2de183e1
Jacob Walls <jwalls@fargeo.com>
2024-06-19 16:06:09 -0400

    nits re #11009

--- tests/fixtures/testing_prj/testing_prj/urls.py
+++ tests/fixtures/testing_prj/testing_prj/urls.py
@@ -3,15 +3,13 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 
-from arches.settings_utils import list_arches_app_names
-
 urlpatterns = [
     path("", include("arches.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Only handle i18n routing in active project. This will still handle the routes provided by Arches core and Arches applications,
 # but handling i18n routes in multiple places causes application errors.
-if settings.APP_NAME != "Arches" and settings.APP_NAME not in list_arches_app_names():
+if settings.APP_NAME == settings.INSTALLED_APPS[-1]:
     if settings.SHOW_LANGUAGE_SWITCH is True:
         urlpatterns = i18n_patterns(*urlpatterns)
 
